1a5373195a09fe28a2f2eda9cbc3f05af4a19b17
devuri <devuriko@gmail.com>
2020-08-09 03:37:56 -0500

    add color variation font_color() and ids for each category

--- src/class-category-list.php
+++ src/class-category-list.php
@@ -17,12 +17,25 @@ class Category_List
 		);
 		$categories = get_terms($terms);
 		foreach ($categories as $key => $category) {
-			$checkbox  = '<div>';
-			$checkbox .= '<input type="checkbox" id="category[]" name="category[]">';
-			$checkbox .= '<label for="scales">'.$category->name.' ('.$category->count.')</label>';
+			$checkbox  = '<div id="category_checkbox" style="padding-bottom: 1em; color:'.self::font_color().';font-weight:600">';
+			$checkbox .= '<input type="checkbox" id="category['.$category->term_id.']" name="category['.$category->term_id.']">';
+			$checkbox .= '<label for="'.sanitize_file_name(strtolower($category->name)).'">'.$category->name.' ('.$category->count.')</label>';
 			$checkbox .= '</div>';
 			echo $checkbox;
 		}
+	}
+
+	/**
+	 * font_color()
+	 * @return [type] [description]
+	 */
+	public static function font_color( bool $restrict = false ){
+		if ($restrict) {
+			$color = '#b9b9b9';
+			return $color;
+		}
+		$color = '#424242';
+		return $color;
 	}
 
 }
