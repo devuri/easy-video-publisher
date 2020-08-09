d1d495790a907f388790a0feeab31c876fc60ee6
devuri <devuriko@gmail.com>
2020-08-09 12:25:33 -0500

    Add class Category_Select

---
+++ src/class-category-select.php
@@ -0,0 +1,58 @@
+<?php
+namespace EasyVideoPublisher;
+
+/**
+ *
+ */
+class Category_Select
+{
+
+	/**
+	 * Custom version of the WP Dropdown Category list
+	 *
+	 * @param  string $fieldname   field name
+	 * @param  array $args define custom arguments
+	 * @return
+	 * @link https://developer.wordpress.org/reference/functions/wp_dropdown_categories/
+	 */
+	public static function categories( $fieldname = null,$args = array()){
+
+		if (current_user_can( 'manage_options' )) {
+			$restricted = '';
+		} else {
+			$restricted = get_option('evp_restricted_categories');
+		}
+
+
+		$catlist_args = array(
+			'show_option_all'    => '',
+			'show_option_none'   => '',
+			'option_none_value'  => '-1',
+			'orderby'            => 'ID',
+			'order'              => 'ASC',
+			'show_count'         => 0,
+			'hide_empty'         => 1,
+			'child_of'           => 0,
+			'exclude'            => $restricted,
+			'echo'               => 0,
+			'selected'           => 0,
+			'hierarchical'       => 0,
+			'name'               => strtolower(str_replace(" ", "_", $fieldname)).'set_category',
+			'id'                 => '',
+			'class'              => 'uk-select',
+			'depth'              => 0,
+			'tab_index'          => 0,
+			'taxonomy'           => 'category',
+			'hide_if_empty'      => false,
+			'value_field'	     => 'term_id',
+		);
+		// ref https://developer.wordpress.org/reference/functions/wp_dropdown_categories/
+		$categories = '<tr class="input-select">';
+		$categories .= '<th><label for="select_dropdown">Select a Category</label></th>';
+		$categories .= '<td>';
+		$categories .= wp_dropdown_categories($catlist_args);
+		$categories .= '</td>';
+		$categories .= '</tr>';
+		return $categories;
+	}
+}
--- video-publisher.php
+++ video-publisher.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Easy Video Publisher
  * Plugin URI:        https://switchwebdev.com/wordpress-plugins/
  * Description:       Easy Video Publisher use to Import Youtube videos from youtube channel playlist or search.
- * Version:           0.7.8
+ * Version:           1.0.4
  * Requires at least: 3.4
  * Requires PHP:      5.6
  * Author:            SwitchWebdev.com
@@ -29,7 +29,7 @@
     }
 
   # plugin directory
-	  define("EVP_VERSION", '0.7.8');
+	  define("EVP_VERSION", '1.0.4');
 
   # plugin directory
     define("EVP_DIR", dirname(__FILE__));
@@ -57,6 +57,9 @@ require_once plugin_dir_path( __FILE__ ). 'src/class-sim-editor.php';
 
 // category list
 require_once plugin_dir_path( __FILE__ ). 'src/class-category-list.php';
+
+// category select
+require_once plugin_dir_path( __FILE__ ). 'src/class-category-select.php';
 
   /**
 	 * Load admin page class via composer
