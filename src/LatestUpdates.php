a6fdd32411d65770d7bd349fc6e47ddf6ba369a6
devuri <devuriko@gmail.com>
2020-08-11 04:09:25 -0500

    remove display()

--- src/LatestUpdates.php
+++ src/LatestUpdates.php
@@ -28,18 +28,5 @@ class LatestUpdates
 	}
 
 
-	public static function display(){
-		foreach ( self::current_updates() as $vkey => $videoId ) {
-
-			$vid_data = YoutubeVideoPost::video_data('https://www.youtube.com/watch?v='.$videoId);
-			$title 			= $vid_data->title;
-			$author 		= $vid_data->author_name;
-			$thumbnail 	= $vid_data->thumbnail_url;
-
-			echo $title.'<hr/>';
-		}
-	}
-
-
 
 }
