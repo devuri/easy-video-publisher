294e842cb91e07c6a92d1a680bdb3d6580d30d9a
devuri <devuriko@gmail.com>
2020-08-11 04:06:22 -0500

    remove YoutubeVideoPost

--- admin/class-easyvidpublisher-admin.php
+++ admin/class-easyvidpublisher-admin.php
@@ -14,7 +14,7 @@ final class VideoPublisherAdmin extends AdminPage {
     $menu = array();
     $menu[] = 'Easy Video Publisher';
     $menu[] = 'Video Publisher';
-    $menu[] = YoutubeVideoPost::access_level('admin');
+    $menu[] = 'manage_options';
     $menu[] = 'easy-video-publisher';
     $menu[] = 'sim_publisher_callback';
     $menu[] = 'dashicons-video-alt3';
