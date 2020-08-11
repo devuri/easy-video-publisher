75ca9a8f17b781d1707210acdf91c0b9cbc99692
devuri <devuriko@gmail.com>
2020-08-11 11:46:15 -0500

    add ChannelImport class

---
+++ src/ChannelImport.php
@@ -0,0 +1,63 @@
+<?php
+namespace EasyVideoPublisher;
+
+/**
+ *
+ */
+class ChannelImport
+{
+
+	public static function publish( string $channelId = null , array $params = array() ){
+
+		/**
+		 * default args
+		 */
+		$default = array();
+		$default['youtube_channel'] = $channelId;
+		$default['number_of_posts'] = 2;
+		$default['setcategory'] 		= array(1);
+		$params = wp_parse_args( $params , $default );
+
+		/**
+		 * get the channel to post from
+		 */
+		$channel 					= trim( $params['youtube_channel'] );
+		$number_of_posts 	= intval( $params['number_of_posts'] );
+		$channel_videos 	= YouTubeAPI::channel_videos( $channel , $number_of_posts );
+
+		# check if we already have the channel_videos in recent_updates
+		$recent_updates 	= get_option('evp_latest_updates');
+		$new_videos 			= array_diff( $channel_videos , $recent_updates);
+		$next_update 			= array_merge( $new_videos , $recent_updates );
+
+		# if we cant find any new videos
+		if ( ! $new_videos ) {
+			return;
+		}
+
+		# create posts
+		foreach ( $new_videos  as $upkey => $vid ) {
+			/**
+			 * check if we posted this already
+			 */
+			$args['tags'] 					= null;
+			$args['category'] 			= $params['setcategory'];
+			$args['create_author']	= false;
+			$id = InsertPost::newpost('https://youtu.be/'.$vid , $args );
+			if ($id) {
+				# get the post id
+				$get_id[] = $id;
+			}
+		}
+
+		# save updates
+		update_option('evp_latest_updates', $next_update );
+
+		/**
+		 * ids for each post
+		 * @var array list of post ids
+		 */
+		return $get_id;
+	}
+
+}
