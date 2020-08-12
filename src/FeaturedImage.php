eb3974233931632e30b05e449763574b0a24e2a6
devuri <devuriko@gmail.com>
2020-08-12 00:07:45 -0500

    add UploadImage for getting images and set as thumbnail, also refactor and add error check

--- src/FeaturedImage.php
+++ src/FeaturedImage.php
@@ -17,14 +17,9 @@ class FeaturedImage
 	 * TODO use oEmbed to get the Image
 	 * @return int 		$attach_id		the attachment id
 	 */
-	public static function set_post_image( $url, $post_id,  $post_title ){
+	public static function setfeatured_image( $thumbnail , $post_id,  $post_title ){
 
-		/**
-		 * set up to use the maxresdefault image
-		 * example https://img.youtube.com/vi/yXzWfZ4N4xU/maxresdefault.jpg
-		 * @link https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
-		 */
-		$image_url = 'https://img.youtube.com/vi/'.$url.'/maxresdefault.jpg';
+		$image_url = $thumbnail;
 
 		/**
 		 * lets make sure all is well
@@ -32,51 +27,31 @@ class FeaturedImage
 		 * if we cant get the high resolution (maxresdefault) use the (hqdefault)
 		 */
 		$get_image = wp_remote_get( $image_url );
- 		if ( $get_image['response']['code'] == 200 ) {
-			// req is ok
- 			$image_url = 'https://img.youtube.com/vi/'.$url.'/maxresdefault.jpg';
- 		} else {
-			$image_url = 'https://img.youtube.com/vi/'.$url.'/hqdefault.jpg';
+		IsError::error_check( $get_image );
+
+ 		if ( ! $get_image['response']['code'] == 200 ) {
+			wp_die( VideoPublisherAdmin::form()->user_feedback( 'I cant download the Image to set featured Image' .' !!!', 'error'));
  		}
 
-		/**
-		 * image details
-		 */
-		$upload_dir = wp_upload_dir();
-		$image_data = @file_get_contents($image_url);
-		$image_name = basename($image_url);
-
-		/**
-		 * split to array
-		 * return file name and extension
-		 * and rename the image to match the post
-		 */
-		$get_img_extension = explode(".", $image_name);
-		$IMGName = $get_img_extension[0];
-		$image_extension = $get_img_extension[1];
-		$img_title = sanitize_text_field( $post_title .' - '. $url );
-		$filename = sanitize_file_name(strtolower($post_title.'-'.$url)).'.'.$image_extension;
-
 		// ok lets upload and stuff
-		if (wp_mkdir_p($upload_dir['path'])) {
-			$file = $upload_dir['path'] . '/' . $filename;
-		} else {
-			$file = $upload_dir['basedir'] . '/' . $filename;
-		}
+		$filename = sanitize_file_name(strtolower($post_title.'-'.uniqid()));
+		$upload_dir = wp_upload_dir();
 
-			/**
-			 * create the post
-			 */
-			@file_put_contents($file, $image_data);
-			$wp_filetype = wp_check_filetype($filename, null );
-			$attachment = array(
-				'post_mime_type' => $wp_filetype['type'],
-				'post_title' => $img_title,
-				'post_content' => '',
-				'post_status' => 'inherit'
-			);
+		/**
+		 * Upload the file and return some info
+		 */
+		$image = UploadImage::upload( $image_url , $upload_dir['path'] , $filename )->save();
+		$file = $image['dirname'] . '/' . $image['basename'];
 
-		$attach_id = wp_insert_attachment( $attachment, $file, $post_id );
+		$wp_filetype = wp_check_filetype($image['basename'], null );
+		$attachment = array(
+			'post_mime_type' => $wp_filetype['type'],
+			'post_title' => $img_title,
+			'post_content' => '',
+			'post_status' => 'inherit'
+		);
+
+		$attach_id = wp_insert_attachment( $attachment, $file , $post_id );
 
 		require_once(ABSPATH . 'wp-admin/includes/image.php');
 			$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
