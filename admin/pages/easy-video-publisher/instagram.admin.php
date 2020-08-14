09317f6d97406b26b0662c84b8d67983e8b311c0
devuri <devuriko@gmail.com>
2020-08-13 22:16:46 -0500

    remove the username

--- admin/pages/easy-video-publisher/instagram.admin.php
+++ admin/pages/easy-video-publisher/instagram.admin.php
@@ -46,7 +46,7 @@ if ( isset( $_POST['submit_post_import'] ) ){
 		$args['category'] 		= intval( trim( $_POST['select_category'] ) );
 		$args['tags'] 				= sanitize_text_field( trim( $_POST['tags'] ) );
 		$args['description']	= wp_filter_post_kses( trim( $_POST['post_description'] ) );
-		$args['hashtags'] 		= array( UrlDataAPI::get_data( $medialink )->author_name .' '. get_term( $args['category'] , 'category' )->name );
+		$args['hashtags'] 		= array( get_term( $args['category'] , 'category' )->name );
 
 
 		/**
