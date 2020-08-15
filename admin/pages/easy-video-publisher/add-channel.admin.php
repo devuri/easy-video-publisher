85e6869b432bc0fcfa4c7191e3c81422b3037b23
devuri <devuriko@gmail.com>
2020-08-15 00:12:41 -0500

    check to make sure the channel has not been added already,

--- admin/pages/easy-video-publisher/add-channel.admin.php
+++ admin/pages/easy-video-publisher/add-channel.admin.php
@@ -1,5 +1,14 @@
 <?php
 
+	use EasyVideoPublisher\YouTubeAPI;
+	use EasyVideoPublisher\SimEditor;
+
+	# make sure we have added channels
+	if ( ! YouTubeAPI::has_key() ) :
+		$adminkeylink = admin_url('/admin.php?page=evp-api-setup');
+		echo $this->form()->user_feedback('Channel Import requires YouTube API Key <strong><a href="'.$adminkeylink.'">Add YouTube API key</a></strong>', 'error');
+	endif;
+
 /**
  * Process the data
  *
@@ -10,40 +19,57 @@ if ( isset( $_POST['add_new_channel'] ) ) :
 		wp_die($this->form()->user_feedback('Verification Failed !!!', 'error'));
 	}
 
-	if ( isset( $_POST['channel_id'] ) && isset( $_POST['channel_name'] ) ) {
+	if ( isset( $_POST['channel_id'] ) ) {
+
 		/**
-		 * Adds new channel
+		 * checks to make sure the request is ok
+		 * if not show the erro message and exit
 		 */
+		if ( ! YouTubeAPI::is_request_ok()  ) {
+			wp_die($this->form()->user_feedback( YouTubeAPI::response_error() .' !!!', 'error'));
+		}
+
+		# Adds new channel
 		$channelId 			= trim( $_POST['channel_id'] );
-		$channelname 		= trim( $_POST['channel_name'] );
+		$channelname 		= YouTubeAPI::channelby_id( $channelId )->snippet->title;
+		$newchannel 		= array( $channelId => $channelname );
 
-		$channels 			= array();
-		$channels 			= get_option( 'evp_channels' );
-		$channels[$channelId] = $channelname;
+		# check if we already have the channels
+		$channels 				= get_option( 'evp_channels' );
+		$is_new						= array_diff( $newchannel , $channels );
+		$update_channels	= array_merge( $newchannel , $channels );
 
-		# add the new channel
-		update_option('evp_channels', $channels );
+		# if we cant find any new videos
+		if ( $is_new ) {
+			# add the new channel
+			update_option('evp_channels', $update_channels );
+			echo $this->form()->user_feedback( '<strong>'.$channelname.'</strong> Channel Added !!!');
+		} else {
+			echo $this->form()->user_feedback('<strong>'.$channelname.'</strong> Channel was already Added !!!', 'error');
+		}
+
 	}
-
 
 endif;
 
-?><h2>
-	<?php _e('Video Publisher Settings'); ?>
-</h2><hr/>
-<div id="yt-importform">
+	# section title
+	SimEditor::section_title('Add YouTube Channels');
+
+?><div id="yt-importform">
 		<form action="" method="POST"	enctype="multipart/form-data"><?php
 		echo $this->form()->table('open');
 		_e('Add Channel');
+
+		#channel ID
 		echo $this->form()->input('Channel ID', ' ');
-		echo $this->form()->input('Channel Name', ' ');
+
 		echo $this->form()->table('close');
 		$this->form()->nonce();
 		echo '<br/>';
 		echo $this->form()->submit_button('Add New Channel', 'primary large', 'add_new_channel');
 	?></form>
 </div><!--frmwrap-->
-<br/><hr/><?php
+<div><br/><hr/><?php
 	/**
 	 * Channels List
 	 * @var [type]
@@ -51,7 +77,9 @@ endif;
 	if ( get_option('evp_channels') ) {
 		_e('Channels');
 		echo '<ul>';
-		foreach ( get_option( 'evp_channels' ) as $chkey => $channel ) {
+		$evp_channels = get_option( 'evp_channels');
+		sort( $evp_channels );
+		foreach ( $evp_channels as $chkey => $channel ) {
 			$ch = '<li style="padding-left:2em;">';
 			$ch .= $channel;
 			$ch .= '</li>';
@@ -62,4 +90,5 @@ endif;
 
 	 ?><p><br>
 		-
-	</p>
+	</p></div>
+</div><!-- flex-form -->
