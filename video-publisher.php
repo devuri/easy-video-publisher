6f7f9a6d80f8adeec74cd057db29fa2c4a60fe74
devuri <devuriko@gmail.com>
2020-08-18 04:35:37 -0500

    1.9.7

--- video-publisher.php
+++ video-publisher.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Video Publisher Pro
  * Plugin URI:        https://switchwebdev.com/wordpress-plugins/
  * Description:       Video Publisher is a easy to use Video import plugin, use to Import Youtube videos from youtube channel playlist or search.
- * Version:           1.9.3
+ * Version:           1.9.7
  * Requires at least: 3.4
  * Requires PHP:      5.6
  * Author:            SwitchWebdev.com
@@ -29,7 +29,7 @@
     }
 
   # plugin directory
-	  define("EVP_VERSION", '1.9.3');
+	  define("EVP_VERSION", '1.9.7');
 
   # plugin directory
     define("EVP_DIR", dirname(__FILE__));
