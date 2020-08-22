d02df9fd53ada7084a7ed86c6505c0c685c2c1c6
devuri <devuriko@gmail.com>
2020-08-22 01:19:48 -0500

    2.1.2

--- src/PostType.php
+++ src/PostType.php
@@ -18,6 +18,7 @@ class PostType
 		);
 		$ptypes = get_post_types( $pt_args, 'objects' );
 
+		// post type array 
 		foreach ( $ptypes as $ptkey => $post_type ) {
 			$post_types[$ptkey] = $post_type->labels->singular_name;
 		}
