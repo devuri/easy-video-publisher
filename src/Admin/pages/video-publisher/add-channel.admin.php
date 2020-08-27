797f2dafe8b21692de05120c920bcebd9158322f
devuri <devuriko@gmail.com>
2020-08-26 23:41:48 -0500

    add option to Delete the Channels, remove the try catch block and refactor channel list

--- src/Admin/pages/video-publisher/add-channel.admin.php
+++ src/Admin/pages/video-publisher/add-channel.admin.php
@@ -23,19 +23,6 @@ if ( isset( $_POST['add_new_channel'] ) ) :
 	if ( isset( $_POST['channel_id'] ) ) {
 
 		/**
-		 * checks to make sure the request is ok
-		 * if not show the error message and exit
-		 */
-		try {
-			YouTubeDataAPI::youtube()->getVideoInfo('YXQpgAAeLM4');
-		} catch (\Exception $e ) {
-			echo $this->form()->user_feedback( 'Request failed: '. $e->getMessage(), 'error');
-			$adminkeylink = admin_url('/admin.php?page=evpro-api-setup');
-			echo $this->form()->user_feedback('Channel Import requires YouTube API Key <strong><a class="button" href="'.$adminkeylink.'">Add YouTube API key</a></strong>');
-		 	return;
-		}
-
-		/**
 		 * get and sanitize new  channel id
 		 */
 		$channel_id = sanitize_text_field( $_POST['channel_id'] );
@@ -64,6 +51,23 @@ if ( isset( $_POST['add_new_channel'] ) ) :
 
 endif;
 
+	/**
+	 * Delete the Channels
+	 *
+	 */
+	if ( isset( $_POST['delete_channels'] ) ) :
+
+		if ( ! $this->form()->verify_nonce()  ) {
+			wp_die($this->form()->user_feedback('Verification Failed !!!', 'error'));
+		}
+
+		# delete channels
+		$delete_channels 	= array();
+		update_option('evp_channels', $delete_channels );
+		echo $this->form()->user_feedback('Channels have been deleted.');
+
+	endif;
+
 	# section title
 	InputField::section_title('Add YouTube Channels');
 
@@ -79,26 +83,12 @@ endif;
 		$this->form()->nonce();
 		echo '<br/>';
 		echo $this->form()->submit_button('Add New Channel', 'primary large', 'add_new_channel');
+		echo '<br>';
+		echo '<br><hr/>';
+
+		echo YouTubeDataAPI::channel_keys();
+		echo '<input name="delete_channels" id="delete_channels" type="submit" class="button" value="Delete Channels ">';
+
 	?></form>
 </div><!--frmwrap-->
-<br/><hr/><?php
-	/**
-	 * Channels List
-	 * @var [type]
-	 */
-	if ( get_option('evp_channels') ) {
-		_e('Channels');
-		echo '<ul>';
-		$evp_channels = (array) get_option( 'evp_channels' );
-		sort( $evp_channels );
-		foreach ( $evp_channels as $chkey => $channel ) {
-			$ch = '<li style="padding-left:2em;">';
-			$ch .= $channel;
-			$ch .= '</li>';
-			echo $ch;
-		}
-		echo '</ul>';
-	}
-
-	 ?><p><br>
-	 </p>
+<br/><hr/>
