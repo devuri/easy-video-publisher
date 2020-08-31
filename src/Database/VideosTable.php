7671d2a1e60559e30258f784c19db51595314782
devuri <devuriko@gmail.com>
2020-08-30 23:50:53 -0500

    update to make compatible with the new abstract class WPDb

--- src/Database/VideosTable.php
+++ src/Database/VideosTable.php
@@ -6,15 +6,15 @@ namespace VideoPublisherPro\Database;
 /**
  *
  */
-class VideosTable extends WPDb
+final class VideosTable extends WPDb
 {
 
 	/**
 	 * set the table name
 	 * @return string
 	 */
-	private static function table_name(){
-		return self::database()->prefix . "evp_videos";
+	protected function table_name(){
+		return $this->database()->prefix . "evp_videos";
 	}
 
 	/**
@@ -23,20 +23,20 @@ class VideosTable extends WPDb
 	 * define the table schema
 	 * @return
 	 */
-	private static function table(){
+	protected function table(){
 
-		$table_name 			= self::table_name();
-		$charset_collate 	= self::database()->get_charset_collate();
+		$table_name 			= $this->table_name();
+		$charset_collate 	= $this->database()->get_charset_collate();
 
 		// schema
 		return "CREATE TABLE {$table_name} (
-		  id bigint(20) unsigned NOT NULL auto_increment,
+		  ID bigint(20) unsigned NOT NULL auto_increment,
 		  post_id bigint(20) unsigned NOT NULL default '0',
 			user_id bigint(20) unsigned NOT NULL DEFAULT '0',
 		  video_id varchar(255),
 		  channel varchar(255),
 		  created datetime NOT NULL default '0000-00-00 00:00:00',
-		  PRIMARY KEY (id),
+		  PRIMARY KEY (ID),
 			KEY post_id (post_id),
 			KEY user_id (user_id),
 			KEY video_id (video_id),
@@ -45,23 +45,12 @@ class VideosTable extends WPDb
 	}
 
 	/**
-	 * create table
-	 *
-	 * @return bool True on success, if the table already exists, False on failure.
+	 * create the table
+	 * @return
 	 */
-	public static function create_table() {
-
-		// be sure to include the file upgrade.php
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-
-		/**
-		 * Create the "evp_videos" Table if it does not exist.
-		 *
-		 * Creates a table in the database, if it doesn’t already exist.
-		 * @link https://developer.wordpress.org/reference/functions/maybe_create_table/
-		 */
-		$table = maybe_create_table( self::table_name() , self::table() );
-		return $table;
+	public static function create_table(){
+		$evp_videos = new VideosTable();
+		$evp_videos->new_table();
 	}
 
 }
