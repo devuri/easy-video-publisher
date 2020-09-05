58dd2d157a5dc6308d5939b9295fe8595bc9fe2f
devuri <devuriko@gmail.com>
2020-09-05 01:21:33 -0500

    doc bloc update

--- src/Database/Videodb.php
+++ src/Database/Videodb.php
@@ -9,11 +9,12 @@ namespace VideoPublisherPro\Database;
 final class Videodb extends GetData
 {
 
-	/**
-	 * check the database if the video id already exists.
-	 *
-	 * @return bool
-	 */
+  /**
+   * check the database if the video id already exists.
+   *
+   * @param null $id
+   * @return bool
+   */
 	public static function video_exists( $id = null ){
 		$videos = self::get_result('video_id');
 		$vid_exists = array_key_exists( $id , $videos );
