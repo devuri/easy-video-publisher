5344cbf937d5142eab972e75c1d0da903a7b57e4
devuri <devuriko@gmail.com>
2020-09-23 21:41:15 -0500

    move list_channels() and keys() to YouTubeData

--- src/YouTube/YouTubeDataAPI.php
+++ src/YouTube/YouTubeDataAPI.php
@@ -236,41 +236,4 @@ class YouTubeDataAPI extends Youtube
 		return $videos;
 	}
 
-	/**
-	 * Get a list of the API keys
-	 *
-	 * @return string API Keys
-	 */
-	public function keys(){
-
-		$keylist 	= '<h4>API Keys:</h4>';
-		$keylist 	.= '<ul style="list-style: decimal;margin-left: 2em;">';
-		foreach( get_option('evp_youtube_api' , array()) as $key => $time ) {
-			$key = substr( $key , 0, -20 );
-			$keylist 	.= '<li><strong>'.$key.'...</strong> Since '.date_i18n( get_option( 'date_format' ), $time ).'</li>';
-		}
-		$keylist 	.= '</ul><br>';
-		return $keylist;
-	}
-
-	/**
-	 * Get a list of channels
-	 *
-	 * @return string list_channels
-	 */
-	public function list_channels(){
-
-		// get the channels
-		$evp_channels =	get_option('evp_channels' , array() );
-		asort($evp_channels);
-
-			$chanlist 	= '<h4>Channels:</h4>';
-			$chanlist 	.= '<ul style="list-style: decimal;margin-left: 2em;">';
-			foreach( $evp_channels as $chkey => $channel ) {
-				$chanlist 	.= '<li><strong>'.$channel.'</strong></li>';
-			}
-			$chanlist 	.= '</ul><br>';
-		return $chanlist;
-	}
-
 }
