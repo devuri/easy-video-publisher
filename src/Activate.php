3e43e8baa571033627ef3e4ae865195e2f4564eb
devuri <devuriko@gmail.com>
2020-10-04 21:04:36 -0500

    new method is_curl_enabled()

--- src/Activate.php
+++ src/Activate.php
@@ -8,6 +8,21 @@ use VideoPublisherlite\Database\VideosTable;
  */
 class Activate
 {
+	/**
+	 * the external class library to get images uses curl,
+	 * so lets prepare incase its not avalable.
+	 *
+	 * This uses curl VideoPublisherlite\Post\ImageUploadFromUrl
+	 * TODO fallback to our own alternative if no curl.
+	 *
+	 * @return boolean
+	 */
+	public static function is_curl_enabled(){
+		if( function_exists('curl_init') === false ){
+			return false;
+		}
+		return true;
+	}
 
 	/**
 	 * create options on activation
