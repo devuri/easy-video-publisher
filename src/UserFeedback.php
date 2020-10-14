dbeef0c2ed7866298eef7116cead961b6d838c45
devuri <devuriko@gmail.com>
2020-10-14 18:18:39 -0500

    phpcs fix

--- src/UserFeedback.php
+++ src/UserFeedback.php
@@ -1,29 +1,26 @@
 <?php
-//TODO change to namespace EvpLite;
+
 namespace VideoPublisherlite;
 
-/**
- *
- */
 class UserFeedback
 {
 
 	/**
-	 * message()
+	 * Message()
 	 *
-	 * give the user some feedback
+	 * Give the user some feedback.
 	 *
-	 * @param  string $class the css class (success | info | warning | error)
-	 * @param  string $message output message
+	 * @param  string $message the css class (success | info | warning | error).
+	 * @param  string $class output message.
 	 * @return string
 	 * @link https://developer.wordpress.org/reference/hooks/admin_notices/
 	 * @link https://developer.wordpress.org/reference/functions/__/
 	 */
-	public static function message($message = 'Options updated', $class = 'success'){
+	public static function message( $message = 'Options updated', $class = 'success' ) {
 
-		$user_message  = '<div style="font-size: small; text-transform: capitalize;" id="user-feedback" class="notice notice-'.$class.' is-dismissible">';
+		$user_message  = '<div style="font-size: small; text-transform: capitalize;" id="user-feedback" class="notice notice-' . $class . ' is-dismissible">';
 		$user_message .= '<p>';
-		$user_message .= __($message);
+		$user_message .= esc_attr( $message );
 		$user_message .= '</p>';
 		$user_message .= '</div>';
 		return $user_message;
