df6b5926a63a64164139a06506740cc82febed49
devuri <devuriko@gmail.com>
2020-11-07 17:30:18 -0500

    phpcs fix

--- src/Admin/pages/video-publisher/channel-import.admin.php
+++ src/Admin/pages/video-publisher/channel-import.admin.php
@@ -7,142 +7,124 @@ use VideoPublisherlite\Form\FormLoader;
 use VideoPublisherlite\Form\InputField;
 use VideoPublisherlite\PostType;
 
-if ( ! defined('ABSPATH') ) exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 	/**
 	 * CSS for the loader
 	 */
 	FormLoader::css_style(
 		array(
-			'size' 				=> '200px',
-			'padding'			=> '1em',
-			'padding-bottom' 	=> '0',
+			'size'           => '200px',
+			'padding'        => '1em',
+			'padding-bottom' => '0',
 		)
 	);
 
-	// make sure we have added channels
+	// make sure we have added channels .
 	if ( ! YouTubeData::api()->has_key() ) :
-		$adminkeylink = admin_url('/admin.php?page=evp-api-setup');
-		echo $this->form()->user_feedback('Channel Import requires YouTube API Key <strong><a href="'.$adminkeylink.'">Add YouTube API key</a></strong>', 'error');
+		$adminkeylink = admin_url( '/admin.php?page=evp-api-setup' );
+		echo $this->form()->user_feedback( 'Channel Import requires YouTube API Key <strong><a href="' . $adminkeylink . '">Add YouTube API key</a></strong>', 'error' ); // @codingStandardsIgnoreLine
 	endif;
 
-	// make sure we have added channels
-	if ( ! get_option('evp_channels') ) :
-	 	echo $this->form()->user_feedback('Please Add at least One Channel', 'warning');
+	// make sure we have added channels .
+	if ( ! get_option( 'evp_channels' ) ) :
+	 	echo $this->form()->user_feedback( 'Please Add at least One Channel', 'warning' ); // @codingStandardsIgnoreLine
 	endif;
 
 /**
  * Process the data
- *
  */
-if ( isset( $_POST['get_latest_updates'] ) ) :
+if ( isset( $_POST['get_latest_updates'] ) ) : // @codingStandardsIgnoreLine
 
-	if ( ! $this->form()->verify_nonce()  ) {
-		wp_die($this->form()->user_feedback('Verification Failed !!!', 'error'));
+	if ( ! $this->form()->verify_nonce() ) {
+		wp_die( $this->form()->user_feedback( 'Verification Failed !!!', 'error' ) ); // @codingStandardsIgnoreLine
 	}
 
 	/**
 	 * Creates the posts
 	 */
-	$posts = ImportVideo::add_video( $_POST );
-
-	if ( $posts ) {
-
-		// display
-		foreach ( $posts as $pkey => $id ) {
-
-			echo $this->form()->user_feedback(
-				get_the_post_thumbnail( $id , 'medium' ) .
-				' <br>New <strong>  '.
-				get_post( $id )->post_status .
-				': <a href="'.get_permalink( $id ).'" target="_blank">' .
-				get_post( $id )->post_title .
-				'</a> </strong> '
-			);
-		}
-	}
+	$create = new ImportVideo( $_POST ); // @codingStandardsIgnoreLine
+	$create->add_video();
+	echo $this->form()->user_feedback( 'Import Has been added to the Queue ! ' ); // @codingStandardsIgnoreLine
 
 endif;
 
-	// section title
-	InputField::section_title('Youtube Channel Import');
+	// section title .
+	InputField::section_title( 'Youtube Channel Import' );
 
-	#loading
-	FormLoader::loading('update-loader');
+	// loading .
+	FormLoader::loading( 'update-loader' );
 
-?><div id="channel-importform">
-		<form action="" method="POST"	enctype="multipart/form-data"><?php
+?>
+<div id="channel-importform">
+		<form action="" method="POST"	enctype="multipart/form-data">
+			<?php
 
-		echo $this->form()->table('open');
+		echo $this->form()->table( esc_attr( 'open' ) ); // @codingStandardsIgnoreLine
 
-		// channel
-		$evp_channels = (array) get_option('evp_channels');
-		asort($evp_channels);
-		echo $this->form()->select( $evp_channels , 'Youtube Channel' );
+		// channel .
+		$evp_channels = (array) get_option( 'evp_channels' );
+		asort( $evp_channels );
+		echo $this->form()->select( $evp_channels, 'Youtube Channel' ); // @codingStandardsIgnoreLine
 
-		// categories
-		echo $this->form()->select( CategoryList::categories() , 'Select Category' );
+		// categories .
+		echo $this->form()->select( CategoryList::categories(), 'Select Category' ); // @codingStandardsIgnoreLine
 
 		/**
 		 * Number of Posts to Create.
-		 * @var array
 		 */
-		$number_of_posts = array_slice(range(0, 20),1, null, true);
-		echo $this->form()->select( $number_of_posts , 'Number of Posts' );
+		$number_of_posts = array_slice( range( 0, 20 ), 1, null, true );
+		echo $this->form()->select( $number_of_posts , 'Number of Posts' ); // @codingStandardsIgnoreLine
 
 		/**
 		 * Posts Types.
-		 * @var array
 		 */
-		if ( current_user_can('manage_options')) :
-			echo $this->form()->select( PostType::post_types() , 'Set Post Type' );
+		if ( current_user_can( 'manage_options' ) ) :
+			echo $this->form()->select( PostType::post_types(), 'Set Post Type' ); // @codingStandardsIgnoreLine
 		endif;
 
 		/**
 		 * Set Post Status.
-		 * @var array
 		 */
 		$post_status = array(
-			'draft'		=> 'Draft',
-			'publish' 	=> 'Publish'
+			'draft'   => 'Draft',
+			'publish' => 'Publish',
 		);
-		echo $this->form()->select( $post_status , 'Post Status' );
+		echo $this->form()->select( $post_status, 'Post Status' ); // @codingStandardsIgnoreLine
 
 		/**
 		 * Set Post Schedule.
-		 * @var array
 		 */
 		$schedule = array(
-			0 	=> 'Now',
-			4 	=> 'Schedule (4h)',
-			8 	=> 'Schedule (8h)',
-			12 	=> 'Schedule (12h)',
-			16 	=> 'Schedule (16h)',
-			24 	=> 'Schedule (24h)',
-			48 	=> 'Schedule (48h)',
+			0   => 'Now',
+			4   => 'Schedule (4h)',
+			8   => 'Schedule (8h)',
+			12  => 'Schedule (12h)',
+			16  => 'Schedule (16h)',
+			24  => 'Schedule (24h)',
+			48  => 'Schedule (48h)',
 			168 => 'Schedule (7d)',
 		);
-		echo $this->form()->select( $schedule , 'Post Schedule' );
+		echo $this->form()->select( $schedule, 'Post Schedule' ); // @codingStandardsIgnoreLine
 
 		/**
 		 * Posts Author.
-		 * @var array
 		 */
 		$set_author = array(
-			0 	=> 'Current Author',
-			1 	=> 'YouTube Author',
+			0 => 'Current Author',
+			1 => 'YouTube Author',
 		);
-		echo $this->form()->select( $set_author , 'Set Author' );
+		echo $this->form()->select( $set_author, 'Set Author' ); // @codingStandardsIgnoreLine
 
 
-		// close the table
-		echo $this->form()->table('close');
+		// close the table .
+		echo $this->form()->table( 'close' ); // @codingStandardsIgnoreLine
 		$this->form()->nonce();
 		echo '<br/>';
-		echo $this->form()->submit_button('Import Videos', 'primary large', 'get_latest_updates');
+		echo $this->form()->submit_button( 'Import Videos', 'primary large', 'get_latest_updates' ); // @codingStandardsIgnoreLine
 
-
-	?></form>
+	?>
+</form>
 </div><!--frmwrap-->
 <br/><hr/>
 <script type="text/javascript">
@@ -152,7 +134,7 @@ endif;
 			$("#new-post-preview").addClass('hidden');
 			$("#channel-importform").addClass('hidden');
 			$(".loading").removeClass('hidden');
-		 });
+		});
 
 	});
 </script>
