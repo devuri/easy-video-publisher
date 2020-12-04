5ed27c71dd2cc5e55f69fd5ef7128b690b6f2376
devuri <devuriko@gmail.com>
2020-12-03 20:47:16 -0500

    feat: add retry Specify the number of times a job should be attempted

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,11 +12,25 @@ class Plugin
 	use Singleton;
 
 	/**
+	 * Retrying Failed Jobs
+	 *
+	 * Specify the number of times a job should be attempted
+	 * before being marked as a failure.
+	 *
+	 * @var int
+	 */
+	protected $retry;
+
+	/**
 	 * Start here
 	 */
 	public function __construct() {
+
+		$this->retry = 3;
+
 		add_action( 'plugins_loaded', array( $this, 'admin_pages' ) );
 		add_action( 'plugins_loaded', array( $this, 'queue' ) );
+
 	}
 
   	/**
@@ -33,11 +47,15 @@ class Plugin
 	/**
 	 * Setup the WP_Queue\Job
 	 *
+	 * Setup queue worker. start cron worker, which piggy backs onto WP cron
+	 * and specify the number of times a job should be attempted before
+	 * being marked as a failure.
+	 *
 	 * @return void
 	 * @link https://github.com/deliciousbrains/wp-queue
 	 */
 	public function queue() {
-		wp_queue()->cron( 3 );
+		wp_queue()->cron( $this->retry );
 	}
 
 }
