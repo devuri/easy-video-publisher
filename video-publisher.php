69d0bde0c6111d228831c17820417044b76cced6
devuri <devuriko@gmail.com>
2021-03-07 10:51:57 -0500

    author name update

--- video-publisher.php
+++ video-publisher.php
@@ -14,8 +14,8 @@
  * Version:           4.0.6
  * Requires at least: 3.4
  * Requires PHP:      7.2.5
- * Author:            SwitchWebdev.com
- * Author URI:        https://switchwebdev.com
+ * Author:            Uriel Wilson
+ * Author URI:        https://urielwilson.com
  * Text Domain:       easy-video-publisher
  * Domain Path:       languages
  * License:           GPLv2
